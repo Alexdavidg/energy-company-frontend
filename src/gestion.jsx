ccac4f7c5ef623722232f475b373db393a4b7a76
Alexdavidg <alegutierrez@utb.edu.co>
2023-11-19 16:21:42 -0500

    actualizacion

--- src/gestion.jsx
+++ src/gestion.jsx
@@ -15,10 +15,10 @@ function Gestion() {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setFormData({
-      ...formData,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
       [name]: value,
-    });
+    }));
   };
 
   const handleSaveData = async () => {
@@ -73,94 +73,91 @@ function Gestion() {
   }, []);
 
   return (
-    <div id='gestionContainer' className="relative w-[100vw] h-fit md:h-[100vh] flex flex-col px-4 bg-background overflow-x-hidden" >
-      <MainNavbar mode="black"/>
+    <div id='gestionContainer' className="relative w-[100vw] h-fit md:h-[100vh] flex flex-col px-4 bg-background overflow-x-hidden">
+      <MainNavbar mode="black" />
       <div className="p-20">
-      <h2 className='title'>Save Data</h2>
-      <form>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            placeholder="Enter name..."
-            required
-          />
+        <h2 className='title'>Save Data</h2>
+        <form>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              placeholder="Enter name..."
+              required
+            />
+          </div>
+          <div className="form-group select-container">
+            <label htmlFor="state">State:</label>
+            <select
+              id="state"
+              name="state"
+              value={formData.state}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="kilometers">Kilometers:</label>
+            <input
+              type="text"
+              id="kilometers"
+              name="kilometers"
+              value={formData.kilometers}
+              onChange={handleInputChange}
+              placeholder="Enter kilometers..."
+              required
+            />
+          </div>
+          <div className="form-group select-container">
+            <label htmlFor="type">Type:</label>
+            <select
+              id="type"
+              name="type"
+              value={formData.type}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="Car">Car</option>
+              <option value="Truck">Truck</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="func">Function:</label>
+            <input
+              type="text"
+              id="func"
+              name="func"
+              value={formData.func}
+              onChange={handleInputChange}
+              placeholder="Enter function..."
+              required
+            />
+          </div>
+          <button className='button' type="button" onClick={handleSaveData}>
+            Save Data
+          </button>
+        </form>
+        <h2 className='title mt-8'>List of Assets</h2>
+        <ul>
+          {assets.map((asset, index) => (
+            <li key={index}>
+              {`${asset.name} - ${asset.state} - ${asset.kilometers} - ${asset.type} - ${asset.func}`}
+              <button className="delete-button" onClick={() => handleDeleteAsset(asset.name)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
 
-        </div>
-        <div>
-          <label htmlFor="state">State:</label>
-          <input
-            type="text"
-            id="state"
-            name="state"
-            value={formData.state}
-            onChange={handleInputChange}
-            placeholder="Enter state..."
-            required
-          />
-
-        </div>
-        <div>
-          <label htmlFor="kilometers">Kilometers:</label>
-          <input
-            type="text"
-            id="kilometers"
-            name="kilometers"
-            value={formData.kilometers}
-            onChange={handleInputChange}
-            placeholder="Enter kilometers..."
-            required
-          />
-
-        </div>
-        <div>
-          <label htmlFor="type">Type:</label>
-          <input
-            type="text"
-            id="type"
-            name="type"
-            value={formData.type}
-            onChange={handleInputChange}
-            placeholder="Enter type..."
-            required
-          />
-
-        </div>
-        <div>
-          <label htmlFor="func">Function:</label>
-          <input
-            type="text"
-            id="func"
-            name="func"
-            value={formData.func}
-            onChange={handleInputChange}
-            placeholder="Enter function..."
-            required
-          />
-
-        </div>
-        <button className='button' type="button" onClick={handleSaveData}>
-          Save Data
-        </button>
-      </form>
-    <h2 className='title mt-8'>List of Assets</h2>
-      <ul>
-        {assets.map((asset, index) => (
-          <li key={index}>
-            {`${asset.name} - ${asset.state} - ${asset.kilometers} - ${asset.type} - ${asset.func}`}
-            <button className="delete-button" onClick={() => handleDeleteAsset(asset.name)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {message && <p className="message">{message}</p>}
-    </div>
+        {message && <p className="message">{message}</p>}
+      </div>
     </div>
   );
 }
